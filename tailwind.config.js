07a6042d734d178b3e76c7304f88b2cf4fafe9ab
Malik Irfan <irfan74077@gmail.com>
2025-12-16 18:30:53 +0500

    add style css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./order/**/*.html",
+    "./products/**/*.html",
+    "./tags/**/*.html",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
